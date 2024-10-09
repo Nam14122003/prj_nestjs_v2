73e3990ecab011ed6fbdf8b8d42a5690e417aa3b
Nam14122003 <147422976+Nam14122003@users.noreply.github.com>
2024-10-09 07:51:04 +0700

    Upload images into db

--- dist/src/modules/post/dto/upload-multi-image.dto.d.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "@/modules/user/entities/user.entity";
-export declare class UploadMultiImageDto {
-    id: number;
-    thumbnail: string;
-    user: User;
-}
--- dist/src/modules/post/dto/upload-multi-image.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UploadMultiImageDto = void 0;
-class UploadMultiImageDto {
-}
-exports.UploadMultiImageDto = UploadMultiImageDto;
-//# sourceMappingURL=upload-multi-image.dto.js.map
